cc4cc0a2e54bff36be24b135f3b87bffce5a7c9a
foolcage <5533061@qq.com>
2020-09-25 12:22:47 +0800

    release 0.0.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except:
 
 setup(
     name='jqdatapy',
-    version='0.0.2',
+    version='0.0.3',
     description='unified,modular quantitative system for human beings ',
     long_description=long_description,
     url='https://github.com/zvtvz/jqdatapy',
