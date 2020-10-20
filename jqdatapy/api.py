8b3dfb0c40c5d6c7f3253748e2a98d5958a0201a
foolcage <5533061@qq.com>
2020-10-20 12:15:06 +0800

    add get_price_period/get_bars_period

--- jqdatapy/api.py
+++ jqdatapy/api.py
@@ -108,6 +108,32 @@ def get_bars(code="600000.XSHG", count=10, unit='1d', end_date=None, fq_ref_date
                           fq_ref_date=fq_ref_date, return_type=return_type, parse_dates=parse_dates)
 
 
+def get_price_period(code="600000.XSHG", unit='1d', date='2005-01-01', end_date=None, fq_ref_date=None,
+                     return_type='df', parse_dates=['date']):
+    """
+
+    :param code: 证券代码
+    :param unit: bar的时间单位, 支持如下周期：1m, 5m, 15m, 30m, 60m, 120m, 1d, 1w, 1M。其中m表示分钟，d表示天，w表示周，M表示月
+    当unit是1w或1M时，第一条数据是开始时间date所在的周或月的行情。当unit为分钟时，第一条数据是开始时间date所在的一个unit切片的行情。
+    最大获取1000个交易日数据
+    :param date: 开始时间，不能为空，格式2018-07-03或2018-07-03 10:40:00，如果是2018-07-03则默认为2018-07-03 00:00:00
+    :param end_date: 结束时间，不能为空，格式2018-07-03或2018-07-03 10:40:00，如果是2018-07-03则默认为2018-07-03 23:59:00
+    :param fq_ref_date: 复权基准日期，该参数为空时返回不复权数据
+    :param return_type:
+    :param parse_dates:
+    :return:
+    """
+    return request_jqdata(method='get_price_period', code=code, unit=unit, date=date, end_date=end_date,
+                          fq_ref_date=fq_ref_date, return_type=return_type, parse_dates=parse_dates)
+
+
+def get_bars_period(code="600000.XSHG", unit='1d', date=None, end_date=None, fq_ref_date=None, return_type='df',
+                    parse_dates=['date']):
+    return get_price_period(code=code, unit=unit, date=date, end_date=end_date, fq_ref_date=fq_ref_date,
+                            return_type=return_type,
+                            parse_dates=parse_dates)
+
+
 def get_token(mob=None, pwd=None, force=False):
     """
 
@@ -192,11 +218,12 @@ if __name__ == "__main__":
     # print(get_all_securities())
     # print(get_trade_days())
     # print(get_trade_days())
-    print(get_fundamentals(count=10, columns='day,pubDate', parse_dates=None))
+    # print(get_fundamentals(count=10, columns='day,pubDate', parse_dates=None))
     # print(get_mtss())
     # print(run_query(count=10, parse_dates=None))
     # print(get_all_securities(code='futures'))
     # print(get_future_contracts())
     # print(get_dominant_future())
     # print(get_bars(code='AU9999.XSGE'))
-    print(get_security_info())
+    # print(get_security_info())
+    print(get_price_period(end_date='2010-01-01'))
